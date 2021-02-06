de02e1ad7c0312ac74f61c1afb79d27c53488dc0
yashpuneet <yash.puneet02@gmail.com>
2021-02-07 02:02:36 +0800

    Added a default value

--- Hello.java
+++ Hello.java
@@ -3,7 +3,13 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello!");
+		String name = "World";
+		if(argv.length != 0)
+		{
+			name = argv[0];
+		}
+
+		System.out.println("Hello, " + name + "!");
 	}
 }
 
