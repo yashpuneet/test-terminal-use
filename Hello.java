bacdc795b525007a07c194af091d08043eabdb56
yashpuneet <yash.puneet02@gmail.com>
2021-02-07 13:06:17 +0800

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello
 	public static void main(String[] args)
 	{
 		//Default is "World"
+		//Author: Yash Puneet (yash.puneet02@gmail.com)
 		String name = "World";
 
 		if(argv.length != 0)
